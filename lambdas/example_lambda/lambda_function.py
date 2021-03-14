98899d483f81ed4dee39b80bf627aa329fbd877d
EldritchCrow <samcohen123456@gmail.com>
2021-03-14 14:55:31 -0400

    Fixed autodeploy CD

--- lambdas/example_lambda/lambda_function.py
+++ lambdas/example_lambda/lambda_function.py
@@ -1,6 +1,7 @@
 
 # Example lambda function
 # The function must be named lambda_handler and take these two objects as arguments
+# The file must be named lambda_function.py
 def lambda_handler(event, context):
     message = 'Hello {} {}!'.format(event['obj_name'], 
                                     event['body'])  
